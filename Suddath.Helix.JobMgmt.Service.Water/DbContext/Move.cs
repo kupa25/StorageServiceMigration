a80dff58faf4a4c45a3253629ab026ce21be73af
Kshitij Upadhyay <Kshitij.Upadhyay@suddath.com>
2020-11-29 20:08:46 -0500

    New Migration list trace line changes

--- Suddath.Helix.JobMgmt.Service.Water/DbContext/Move.cs
+++ Suddath.Helix.JobMgmt.Service.Water/DbContext/Move.cs
@@ -24,6 +24,7 @@ namespace Suddath.Helix.JobMgmt.Services.Water.DbContext
         public string TRAFFIC_MANAGER { get; set; }
         public string BILLER { get; set; }
         public string QUOTED_BY { get; set; }
+        public string SALES { get; set; }
         public DateTime? READY_TO_ACCRUE_DATE { get; set; }
         public DateTime? ACCRUED_DATE { get; set; }
 
